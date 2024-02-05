95903995ab0d2885f2efa8c822fb8b863b1fdb68
Ilya Gusev <phoenixilya@gmail.com>
2024-02-05 22:04:00 +0000

    Basic websocket

--- gui/static/js/index.js
+++ gui/static/js/index.js
@@ -3,6 +3,41 @@
 window.PIXI = PIXI;
 
 const cubism4Model = "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/test/assets/haru/haru_greeter_t03.model3.json";
+const DEFAULT_WS_ADDR = "ws://159.69.16.3:5000/ws";
+
+function bindSocket(socket, model) {
+    socket.onmessage = (event) => {
+        let data;
+        try {
+            data = JSON.parse(event.data);
+        } catch (error) {
+            console.log('ws onmessage error:', error);
+            return;
+        }
+        if (!data) {
+            return;
+        }
+        if (data.model) {
+            console.log('ws onmessage data.model:', data.model);
+            model.setModel(data.model);
+        }
+        if (data.motion) {
+            console.log('ws onmessage data.motion:', data.motion);
+            if (typeof data.motion === 'string') {
+                data.motion = {
+                    group: data.motion,
+                };
+            }
+            model.setMotion(data.motion);
+        }
+        if (data.expression) {
+            console.log('ws onmessage data.expression:', data.expression);
+            model.expression(data.expression);
+        }
+    };
+};
+
+const socket = new WebSocket(DEFAULT_WS_ADDR);
 
 (async function main() {
   const app = new PIXI.Application({
@@ -12,10 +47,13 @@ const cubism4Model = "https://cdn.jsdelivr.net/gh/guansss/pixi-live2d-display/te
   });
 
   const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
+  socket.send("!!!");
+  bindSocket(socket, model4);
 
   app.stage.addChild(model4);
 
   model4.scale.set(0.25);
-  model4.x = 100;
+  model4.x = 200;
+  //model4.expression(0)
 })();
 
--- run.py
+++ run.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+import random
+import asyncio
+import json
+import time
+
+from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await websocket.send_text(json.dumps({"expression": random.randint(0, 5)}))
+        await asyncio.sleep(5)
 
 app.mount("/", StaticFiles(directory="gui", html=True), name="gui")
